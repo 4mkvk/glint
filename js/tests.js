a26390c0ca536f17ed1e0985ed610ace0dfdc11f
Арлан Аманбаев <@corp.itstep>
2021-05-03 19:53:45 +0600

    filter by umni aarlan

--- js/tests.js
+++ js/tests.js
@@ -4,10 +4,10 @@ console.log(users)
 
 let tests = JSON.parse(localStorage.getItem("testsArray"))
 console.log(tests)
-for(let i = 0; i < tests.length; i++){
+for (let i = 0; i < tests.length; i++) {
     let testCreator = null;
-    for(let j=0;j<users.length;j++){
-        if(users[j]['userId'] === tests[i]['userId']){
+    for (let j = 0; j < users.length; j++) {
+        if (users[j]['userId'] === tests[i]['userId']) {
             testCreator = users[j].userName
         }
     }
@@ -30,14 +30,43 @@ for(let i = 0; i < tests.length; i++){
     `)
 }
 
-function changesCategory () {
+function changesCategory() {
     $("#testsBlock").empty();
     let category = $("#category").val();
-    for (let i = 0; i < tests.length; i++) {
-        if(category ===tests[i].testCategory){
-            drawCard(tests[i].testName, tests[i].testCategory)
+
+    if (category === 'Все') {
+        location.reload()
+    } else{
+        for (let i = 0; i < tests.length; i++) {
+            if (category === tests[i].testCategory) {
+                let testCreator = null;
+                for (let j = 0; j < users.length; j++) {
+                    if (users[j]['userId'] === tests[i]['userId']) {
+                        testCreator = users[j].userName
+                    }
+                }
+                testsBlock.append(`
+                    <div class="testCard">
+                        <div class="testCardImageBlock">
+                            <img src="./img/test-card.png" alt="" class="testCardImage">
+                        </div>
+                            <div class="testCardContent">
+                                <div class="testCardTitleBlock">
+                                    <span class="testCardTitle">${tests[i].testName}</span>
+                                    <span class="testCardSpan">10 вопросов</span>
+                                </div>
+                                <div class="testCardDownBlock">
+                                    <button class="testCardTakeTest" onclick="takeTheTest()">Пройти</button>
+                                    <span class="testCardSpan">От ${testCreator}</span>
+                                </div>
+                        </div>
+                    </div>
+                    `)
+            }
         }
     }
+
+    
 }
 
 function takeTheTest() {
